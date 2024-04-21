8917dbdc3c9255505910b281d2a0e753c9af3647
AlvaroBT7 <barreroalvaro2007@gmail.com>
2024-04-21 14:10:13 +0200

    Arreglo de importaciones y contador de lineas de tres digitos

--- src/utils/fetch-code.ts
+++ src/utils/fetch-code.ts
@@ -1,6 +1,3 @@
-import fromStringToCode from "./code-coloring";
-import { Tab } from "./definitions";
-
 const fetchCode = async (filename: string): Promise<string> => {
   const response = await fetch(`/code/${filename}`);
   const text = await response.text();
